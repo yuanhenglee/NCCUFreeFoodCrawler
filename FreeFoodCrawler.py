8e9e5fd4a3eb7944ec111e993f322f9fc66a93d1
yuanhenglee <yuanhenglee2000@gmail.com>
2020-12-14 21:22:32 +0800

    VERSION 12//14

---
+++ FreeFoodCrawler.py
@@ -0,0 +1,34 @@
+import requests
+from bs4 import BeautifulSoup
+
+url_head = "https://moltke.nccu.edu.tw/Registration/"
+matches = ['食物','餐盒','午餐','晚餐','供餐','點心']
+
+def getSoup(url):
+    req = requests.get(url)
+    soup = BeautifulSoup(req.text,'html.parser')
+    return soup
+
+def checkFood(short_url):
+    soup = getSoup( url_head + str(short_url))
+    # print(soup.prettify())
+    for paragraph in soup.findAll('p'):
+        content = paragraph.text.strip()
+        if any(x in content for x in matches):
+            return short_url
+    return None
+
+# new event webpage
+soup = getSoup("https://moltke.nccu.edu.tw/Registration/registration.do?action=new")
+
+# Get all events
+conference_list = []
+for h4 in soup.findAll('h4'):
+    for a in h4.findAll('a'):
+        conference_list.append(a['href'])
+
+for i in conference_list:
+    if(checkFood(i)):
+        print( url_head + str(i))
+
+
